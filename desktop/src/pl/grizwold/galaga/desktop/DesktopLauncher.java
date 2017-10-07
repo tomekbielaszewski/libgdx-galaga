0060cf41a3cfaca18a784fa9c257fb87bce6ba63
Grizwold <tomekbielaszewski@gmail.com>
2017-10-07 19:35:35 +0200

    Initial commit

---
+++ desktop/src/pl/grizwold/galaga/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.grizwold.galaga.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.grizwold.galaga.GalagaGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GalagaGame(), config);
+	}
+}
